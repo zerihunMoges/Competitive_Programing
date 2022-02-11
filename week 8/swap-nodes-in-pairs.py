a831a26fe47295666d478b00043260a3868bd073
zerihunttt655674 <91532718+zerihunttt655674@users.noreply.github.com>
2022-02-11 17:16:01 +0300

    Update swap-nodes-in-pairs.py

--- week 8/swap-nodes-in-pairs.py
+++ week 8/swap-nodes-in-pairs.py
@@ -1,3 +1,27 @@
+
+        #Recursion
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None or head.next == None:
+            return head
+        else:
+            
+            
+            nxt = head.next
+            nxtnxt = nxt.next
+            nxt.next = head
+            
+            head.next = self.swapPairs(nxtnxt)
+            return nxt
+       
+
+    #Loop
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
