358bb1ce286f4ebcc0669ad50e00b34d279656d7
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-11-16 14:59:59 +0300

    Time: 1870 ms (16.62%), Space: 83.1 MB (8.81%) - LeetHub

---
+++ 0818-race-car/0818-race-car.py
@@ -0,0 +1,27 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+        memo = defaultdict(int)
+        moves = deque([[0,1, 0]])
+        visited = set()
+        while moves:
+            
+            pos, speed, move = moves.popleft()
+            if pos == target:
+                return move
+            
+            a_pos, a_speed = pos+speed, speed*2
+            r_pos, r_speed = pos, -1*(abs(speed)//speed)
+        
+            if -target <= a_pos <= 2*target and (a_pos, a_speed) not in visited:
+                visited.add((a_pos, a_speed))
+                moves.append([a_pos, a_speed, move+1])
+                
+            if -target <= r_pos <= 2*target and (r_pos, r_speed) not in visited:
+                visited.add((r_pos, r_speed))
+                moves.append([r_pos, r_speed, move+1])
+                
+        return -1
+        
+        
+        
+                
