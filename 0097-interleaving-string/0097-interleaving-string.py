c82fe0e5bd3795d6366be81bc006020e595f0361
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-11-22 11:21:02 +0300

    Time: 129 ms (16.11%), Space: 14 MB (90.92%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,22 +2,25 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
      
         
-        dp = [[0]*(len(s1)+1) for i in range(len(s2)+1)]
+        dp = [0]*(len(s1)+1)
         
-        for i in range(len(dp)):
-            for j in range(len(dp[i])):
-
+        for i in range(len(s2)+1):
+            prev = 0
+            for j in range(len(s1)+1):
+                cur_best = 0
                 if 0 <= i+j-1 < len(s3):
-                    cur_best = 0
+                    
                     if j > 0 and s1[j-1] == s3[i+j -1]:
-                        cur_best = max(cur_best, dp[i][j-1] + 1)
+                        cur_best = max(cur_best, prev + 1)
                     
                     if i > 0 and s2[i-1] == s3[i+j -1]:
-                        cur_best = max(cur_best, dp[i-1][j] + 1)
+                        cur_best = max(cur_best, dp[j] + 1)
                   
-                    dp[i][j] = cur_best
-       
-        return dp[-1][-1] == len(s3)
-                
+                dp[j] = cur_best
+                prev = cur_best
+      
+        return dp[-1] == len(s3)
+     
+        
      
         
