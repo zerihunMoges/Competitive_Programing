223355e39350483991f8a267a0a4546a90281056
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-11-22 11:09:07 +0300

    Time: 178 ms (5.52%), Space: 14.2 MB (60.92%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,24 +1,26 @@
-class Solution:
-    def interleave(self, s1idx, s1, s2idx, s2, s3, memo):
-        
-        if (s1idx, s2idx) in memo:
-            return False
-        
-        if s1idx >= len(s1) and s2idx >= len(s2) and s1idx+s2idx >= len(s3):
-            return True
-        
-        if s1idx+s2idx >= len(s3):
-            return False
-        
-        if s1idx < len(s1) and s1[s1idx] == s3[s1idx+s2idx] and self.interleave(s1idx+1,s1, s2idx,s2, s3, memo):
-            return True
-        if s2idx < len(s2) and s2[s2idx] == s3[s1idx+s2idx] and self.interleave(s1idx,s1, s2idx + 1,s2, s3, memo):
-            return True
-        
-        memo.add((s1idx, s2idx))
-        return False
-        
+class Solution: 
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        memo = set()
-        return self.interleave(0, s1, 0, s2, s3, memo)
+     
+        
+        dp = [[0]*(len(s1)+1) for i in range(len(s2)+1)]
+        
+        for i in range(len(dp)):
+            for j in range(len(dp[i])):
+
+                if 0 <= i+j-1 < len(s3):
+                    cur_best = 0
+                    if j > 0 and s1[j-1] == s3[i+j -1]:
+                        cur_best = max(cur_best, dp[i][j-1] + 1)
+                    
+                    if i > 0 and s2[i-1] == s3[i+j -1]:
+                        cur_best = max(cur_best, dp[i-1][j] + 1)
+                  
+                    dp[i][j] = cur_best
+                    
+        for row in dp:
+            print(row)
+       
+        return dp[-1][-1] == len(s3)
+                
+     
         
