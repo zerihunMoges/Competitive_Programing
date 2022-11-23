b1c6c541855d47e12eb234083e677da095b6881d
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-11-23 18:34:34 +0300

    Time: 101 ms (29.47%), Space: 14.6 MB (50.30%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -16,7 +16,18 @@ class Solution:
         
     def maxProfit(self, prices: List[int]) -> int:
         
-        haveStock = False
+       
+        states = 2
         day = 0
-        memo = defaultdict(int)
-        return self.getMaxProfit(haveStock, day, memo, prices)
+        memo = [[0]*(states) for i in range(len(prices)+2)]
+        for day in reversed(range(len(memo)-2)):
+            for state in range(states):
+                
+                if state == 0:
+                    memo[day][state] = max(-prices[day] + memo[day+1][state+1], memo[day+1][state])
+                else:
+                    memo[day][state] = max(prices[day] + memo[day+2][state-1], memo[day+1][state])
+             
+        return memo[0][0]
+                    
+                    
