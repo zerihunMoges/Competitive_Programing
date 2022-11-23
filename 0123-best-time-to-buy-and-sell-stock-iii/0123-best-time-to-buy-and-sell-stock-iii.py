f69f3dc4da54366e3719f4096e6cbeef2d8bb274
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-11-23 11:27:39 +0300

    Time: 7888 ms (5.33%), Space: 55.6 MB (17.79%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -16,14 +16,29 @@ class Solution:
         return memo[(haveStock, transactions, day)]
         
     def maxProfit(self, prices: List[int]) -> int:
-        memo = defaultdict(int)
-        haveStock = False
-        transactions = 0
-        day = 0
+        transactions = 2
+        states = 2
         
-        profit = self.findMaxProfit(haveStock, transactions, day, prices, memo)
-
-                 
+        memo = [[[0]*(transactions+1) for i in range(states)] for j in range(len(prices)+1)]
         
-        return profit
+        
+        
+        for day in reversed(range(len(memo)-1)):
+            for state in range(len(memo[day])):
+                for transaction in range(1, len(memo[day][state])):
+                    if state == 1:
+                        memo[day][state][transaction] = max(prices[day]+memo[day+1][state-1][transaction-1], memo[day+1][state][transaction] )
+                        
+                    else:
+                        memo[day][state][transaction] = max(-prices[day]+memo[day+1][state+1][transaction], memo[day+1][state][transaction] )
+        
+        
+        return max(memo[0][0])
+                        
+       
+        
+        
+        
+        
+        
         
