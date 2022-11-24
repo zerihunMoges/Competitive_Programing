7f2f188c0f9b7b8e2d957f57a7acdb023c4722ec
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-11-24 10:09:18 +0300

    Time: 4704 ms (38.47%), Space: 154.7 MB (42.13%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -2,20 +2,16 @@ class Solution:
     def findLongestPath(self, cur, neigs, s):
     
         path, longest_path = 1, 1
-
         longest_child_path = 0
 
         for neig in neigs[cur]:
             child_path, child_longest_path = self.findLongestPath(neig, neigs, s)
 
             if s[cur] != s[neig]:
-
                 longest_path = max(longest_child_path + 1 + child_path, longest_path)
                 longest_child_path = max(longest_child_path, child_path)
-
-
             longest_path = max(longest_path, child_longest_path, child_path)
-
+            
         return 1+longest_child_path, longest_path
     
     
