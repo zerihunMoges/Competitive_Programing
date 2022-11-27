09584ce9479e81311239fc2c81532b859a8d5de1
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-11-27 20:45:49 +0300

    Time: 2892 ms (6.30%), Space: 757.4 MB (7.65%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
@@ -14,9 +14,22 @@ class Solution:
     
     def countHousePlacements(self, n: int) -> int:
         ways = self.countWays(n, False)
-        
-        
-        return (ways**2)%(10**9+7)
+        ways = [[0,0] for i in range(n+1)]
+        top = 1
+        bottom = 1
+        for i in range(1, len(ways)):
+            tempt = 0
+            tempb = 0
+            for j in range(len(ways[i])):
+                if j == 0:
+                    tempt += bottom
+                    tempt += top
+                else:
+                    tempb += top
+            top = tempt
+            bottom = tempb
+                
+        return (top**2)%(10**9+7)
         
         
 
