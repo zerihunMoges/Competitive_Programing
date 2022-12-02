2fee9abba55f92508a3cfd674c299ea4555c2481
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-12-03 01:30:36 +0300

    Time: 57 ms (60.42%), Space: 13.9 MB (78.95%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,22 +1,23 @@
 class Solution:
     
-    def findPermutation(self, n,  target, idx, available, prev):
-        if idx < 0:
-            return ''
-        cur = math.factorial(idx)+prev
-        nums = sorted(available)
-        i = 0
-        while cur < target:
-            
-            cur += math.factorial(idx)
-            i += 1
-            
-        available.remove(nums[i])
-        return str(nums[i]) + self.findPermutation(n, target, idx-1, available, cur-math.factorial(idx))
-    
-        
     def getPermutation(self, n: int, k: int) -> str:
         
-        perm = self.findPermutation(n, k, n-1, set([i for i in range(1, n+1)]), 0)
-        return perm
+        available = set([i for i in range(1, n+1)])
+        answer = ''
+        prev = 0
+        for j in reversed(range(n)):
+            cur = math.factorial(j)+prev
+            nums = sorted(available)
+            i = 0
+            while cur < k:
+
+                cur += math.factorial(j)
+                i += 1
+
+            available.remove(nums[i])
+            
+            answer += str(nums[i])
+            prev = cur-math.factorial(j)
+        
+        return answer
         
