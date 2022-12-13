dcb1c08a6ee0ba15b6ae80ed37d9ec4d15c23492
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-12-13 20:16:28 +0300

    Time: 155 ms (42.25%), Space: 14.2 MB (89.12%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.py
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -13,19 +13,8 @@ class Solution:
         return path
             
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        
-        graph = defaultdict(list)
-        
-        for word in wordList:
-            
-            new_word = list(word)
-            for i in range(len(word)):
-                for j in range(ord('a'), ord('z')+1):
-                    og = word[i]
-                    new_word[i] = chr(j)
-                    graph[''.join(new_word)].append(word)
-                    new_word[i] = og
-        
+
+        wordList = set(wordList)
         words = deque([beginWord])
         path = defaultdict(list)
         visited = set()
@@ -39,13 +28,21 @@ class Solution:
                     answer.append([cur])
 
 
-                for neig in graph[cur]:
+                new_word = list(cur)
+                for i in range(len(new_word)):
+                    for j in range(ord('a'), ord('z')+1):
+                        og = cur[i]
+                        new_word[i] = chr(j)
+                        neig = ''.join(new_word)
+                        if neig in wordList:
                     
-                    if neig not in visited and neig not in temp:
-                        temp.add(neig)
-                        words.append(neig)
-                    if neig not in visited:
-                        path[neig].append(cur)
+                            if neig not in visited and neig not in temp:
+                                temp.add(neig)
+                                words.append(neig)
+                            if neig not in visited:
+                                path[neig].append(cur)
+                            
+                        new_word[i] = og
                         
             visited = visited.union(temp)
         
