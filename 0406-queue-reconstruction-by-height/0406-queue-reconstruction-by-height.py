6b147352fdbe0b7129a97460a4cd9501b8b3a14e
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-12-22 10:26:21 +0300

    Time: 100 ms (93.05%), Space: 14.3 MB (94.60%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
@@ -1,33 +1,11 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         
-   
-        peoples = []
-        
-        for h,k in people:
-            peoples.append([h,k])
-            
-        queue = []
-        for i in range(len(peoples)):
-            front = None
-            for j in range(len(peoples)):
-                
-                h , k = peoples[j]
-                
-                if k == 0 and ( front == None or h < peoples[front][0]):
-
-                    front = j
-     
-            for j in range(len(peoples)):
-
-                if peoples[front][0] >= peoples[j][0]:
-                    peoples[j][1] -= 1
-
-            queue.append(people[front])
-
-            peoples[front] = [float('inf'), float('inf')]
-  
-        return queue
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
             
         
         
