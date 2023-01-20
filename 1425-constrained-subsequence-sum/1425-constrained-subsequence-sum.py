0c2421cc772eaddfb3412210921f903e44469026
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-01-20 21:35:09 +0300

    Time: 3516 ms (31.39%), Space: 35.9 MB (5.23%) - LeetHub

---
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        
+        heap = []
+        maxsum = -float('inf')
+        for i in range(len(nums)):  
+            prev_max = 0
+            if heap:
+                prev_max = max(prev_max, -heap[0][0])
+            heapq.heappush(heap, [-(nums[i]+prev_max), i])
+            while heap and (i - heap[0][1] >= k or i == len(nums)-1):
+                tot, index = heapq.heappop(heap)
+                maxsum = max(-tot, maxsum)
+                
+        return maxsum
+                
+        
+            
