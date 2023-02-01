95c5c1bd447725347c7710251f100c8e90491b48
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-02-01 16:51:54 +0300

    Time: 1963 ms (63.64%), Space: 14.4 MB (23.08%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,21 +1,4 @@
 class Solution:
-    def bestTeam(self,i , min_i, scores, memo):
-        if i >= len(scores):
-            return 0
-        
-        if (i, min_i) in memo:
-            return memo[(i, min_i)]
-        
-        pick = 0
-        skip = 0
-        
-        if scores[i][0] >= scores[min_i][0]:
-            pick = scores[i][0] + self.bestTeam(i+1, i , scores, memo)
-            
-        skip = self.bestTeam(i+1, min_i, scores, memo)
-        memo[(i, min_i)] = max(pick, skip)
-        return memo[(i, min_i)]
-        
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
         
         some = [[scores[i], ages[i]] for i in range(len(scores))]
