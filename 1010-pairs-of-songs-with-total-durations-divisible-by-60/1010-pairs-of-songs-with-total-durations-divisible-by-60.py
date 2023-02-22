208d3bbb414b9efa221ba8efd2614a73fb283ca4
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-02-22 21:48:55 +0300

    Time: 575 ms (11.66%), Space: 17.7 MB (65.46%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -6,7 +6,7 @@ class Solution:
         tot = 0
         for t in time:
             
-            for i in range(60, 1000, 60):
+            for i in range(60, t+561, 60):
                 tot += count[i-t]
             count[t] += 1
                 
