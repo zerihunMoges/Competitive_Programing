b2c2b7a6f4c8d4d4c0b4bbdbe32bf1bb72a3e537
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-02-24 18:27:00 +0300

    Time: 364 ms (72.73%), Space: 19.9 MB (95.45%) - LeetHub

--- 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.py
+++ 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.py
@@ -1,15 +1,19 @@
 class TweetCounts:
 
     def __init__(self):
-        self.tweets = dict()
+        self.tweets = defaultdict(list)
 
     def recordTweet(self, tweetName: str, time: int) -> None:
-        self.tweets.setdefault(tweetName, []).append(time)
+        self.tweets[tweetName].append(time)
 
     def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
-        if freq == "minute": seconds = 60 
-        elif freq == "hour": seconds = 3600
-        else: seconds = 86400
+        
+        if freq == "minute": 
+            seconds = 60 
+        elif freq == "hour": 
+            seconds = 3600
+        else: 
+            seconds = 86400
         
         ans = [0] * ((endTime - startTime)//seconds + 1)
         for t in self.tweets[tweetName]:
