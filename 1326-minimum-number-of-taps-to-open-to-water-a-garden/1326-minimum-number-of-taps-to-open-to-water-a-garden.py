d13c9095daf2f8de0a4426a198f984e7c53a78d5
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-02-24 21:10:18 +0300

    Time: 970 ms (10.19%), Space: 14.4 MB (71.05%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -0,0 +1,14 @@
+class Solution:
+    
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        
+        mintap = [0] + [float('inf')] * n
+        
+        for i in range(len(ranges)):
+            rng = ranges[i]
+            for j in range(max(i - rng + 1, 0), min(i + rng, n) + 1):
+                mintap[j] = min(mintap[j], mintap[max(0, i - rng)] + 1)
+                
+        return mintap[n] if mintap[n] != float('inf') else -1
+    
+    
