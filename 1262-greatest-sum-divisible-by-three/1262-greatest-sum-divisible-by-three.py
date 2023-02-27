67cbea8d94b108ac8ba7d7cc959726cf0e2b68e8
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-02-27 12:59:44 +0300

    Time: 379 ms (60.36%), Space: 19.7 MB (79.28%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        
+        dp = [0, 0, 0]
+        for num in nums:
+            for i in dp[:]:
+                dp[(i + num) % 3] = max(dp[(i + num) % 3], i + num)
+                
+        return dp[0]
+                
