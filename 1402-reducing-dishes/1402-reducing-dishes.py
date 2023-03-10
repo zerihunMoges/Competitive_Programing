4ab673eccf83f750188c5d7cb4bbc3b4199a7d79
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-03-10 15:07:04 +0300

    Time: 1145 ms (5.20%), Space: 39.8 MB (11.23%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxS(self, time, index, satisfaction, memo):
+        
+        if index >= len(satisfaction):
+            return 0
+        if (index, time) in memo:
+            return memo[(index, time)]
+        skip = self.findMaxS(time, index+1, satisfaction, memo)
+        make = time*satisfaction[index] + self.findMaxS(time+1, index+1, satisfaction, memo)
+        
+        memo[(index, time)] = max(skip, make)
+        return memo[(index, time)]
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        
+        satisfaction.sort()
+        
+        return self.findMaxS(1, 0, satisfaction, defaultdict(int))
