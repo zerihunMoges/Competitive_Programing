55ecc6f181bac745be57116bd33aa1d4606151d6
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-07-08 22:14:19 +0300

    Time: 783 ms (36.28%), Space: 30 MB (45.13%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -1,16 +1,13 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
         
-        
-        
-        
-        
         border = []
         
         for i in range(len(weights)-1):
             border.append(weights[i]+weights[i+1])
             
         border.sort()
+        
         difference = 0
         for i in range(k-1):
             difference += border[-i-1]
